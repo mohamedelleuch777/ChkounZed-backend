16e54431644973e35a85c7c8282bffe062d89310
mohamedelleuch777 <usher7med@gmail.com>
2021-11-01 01:53:36 +0300

    added user controller

--- controllers/test.php
+++ controllers/test.php
@@ -2,6 +2,10 @@
 
 class Test {
     function __construct() {
-        echo "hello world'!!!";
+        echo "hello world'!!!<br />";
+    }
+
+    public function hi() {
+        echo 'this is the hi method executed!!!';
     }
 }
---
+++ controllers/users.php
@@ -0,0 +1,23 @@
+<?php
+
+class  Users {
+    function __construct () {
+
+    }
+
+    public function CreateUser() {
+        echo 'call Users.CreateUser()';
+    }
+
+    public function UpdateUser() {
+        echo 'call Users.UpdateUser()';
+    }
+
+    public function RemoveUser() {
+        echo 'call Users.RemoveUser()';
+    }
+
+    public function GetUserData() {
+        echo 'call Users.GetUserData()';
+    }
+}
