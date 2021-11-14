49dcad3428cfdb29920914c67d9b0e64cf591545
mohamedelleuch777 <usher7med@gmail.com>
2021-11-14 05:48:14 +0300

    working on remote server (ionos)

--- index.py
+++ index.py
@@ -18,6 +18,60 @@ requestedController = config.importModule("controllers/"+requestedClassName.lowe
 if requestedController == None:
     config.Controller.InexistantRequest()
 requestedClass = getattr(requestedController, requestedClassName)
+#print ([func for func in dir(requestedClass) if callable(getattr(requestedClass, func))])
+try:
+    myClass = requestedClass()
+except Exception as e:
+    config.ThrowException(e)
+try:
+    methodToCall = getattr(myClass, requestedMethodName)
+except Exception as e:
+    config.Controller.InexistantRequest()
+
+result = methodToCall()
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/usr/bin/env python
+import config
+
+
+if len(config.REQUEST_URL.split('/')) <= 2:
+    config.Controller.BlockRequests()
+if config.REQUEST_URL.split('/')[2] == '':
+    config.Controller.BlockRequests()
+if len(config.REQUEST_URL.split('/')) > 3:
+    config.Controller.InexistantRequest()
+requestedClassName = config.REQUEST_URL.split('/')[1]
+requestedMethodName = config.REQUEST_URL.split('/')[2]
+requestedArgs = config.REQUEST_ARGS
+
+
+
+requestedController = config.importModule("controllers/"+requestedClassName.lower()+".py")
+if requestedController == None:
+    config.Controller.InexistantRequest()
+requestedClass = getattr(requestedController, requestedClassName)
 myClass = requestedClass()
 try:
     methodToCall = getattr(myClass, requestedMethodName)
